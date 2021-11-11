dbe8114dad8c0c19d4eb06ee3988cc59ff4c0520
Chaemin-153 <0520032@naver.com>
2021-11-11 15:14:35 +0900

    Add Edit Sheet

--- src/controllers/musicController.js
+++ src/controllers/musicController.js
@@ -25,5 +25,34 @@ export const postUploadMusic = async (req, res) => {
 export const seeMusic = async (req, res) => {
 	const { id } = req.params;
 	const sheet = await Sheet.findById(id);
+	if (!sheet) {
+		return res.render("404", { pageTitle: "Sheet not found." });
+	}
 	return res.render("see-music", { pageTitle: sheet.title, sheet });
+};
+
+export const getEditMusic = async (req, res) => {
+	const { id } = req.params;
+	const sheet = await Sheet.findById(id);
+	if (!sheet) {
+		return res.render("404", { pageTitle: "Sheet not found." });
+	}
+	return res.render("edit-sheet", { pageTitle: `Edit: ${sheet.title}`, sheet });
+};
+
+export const postEditMusic = async (req, res) => {
+	const { id } = req.params;
+	const { title, description, hashtags } = req.body;
+	const sheet = await Sheet.exists({ _id: id });
+	if (!sheet) {
+		return res.render("404", { pageTitle: "Sheet not found." });
+	}
+	await Sheet.findByIdAndUpdate(id, {
+		title,
+		description,
+		hashtags: hashtags
+			.split(",")
+			.map((word) => (word.startsWith("#") ? word : `#${word}`)),
+	});
+	return res.redirect(`/music/${id}`);
 };
--- src/routers/musicRouter.js
+++ src/routers/musicRouter.js
@@ -1,6 +1,8 @@
 import express from "express";
 import {
+	getEditMusic,
 	getUploadMusic,
+	postEditMusic,
 	postUploadMusic,
 	seeMusic,
 } from "../controllers/musicController";
@@ -9,5 +11,9 @@ const musicRouter = express.Router();
 
 musicRouter.get("/:id([0-9a-f]{24})", seeMusic);
 musicRouter.route("/upload").get(getUploadMusic).post(postUploadMusic);
+musicRouter
+	.route("/:id([0-9a-f]{24})/edit")
+	.get(getEditMusic)
+	.post(postEditMusic);
 
 export default musicRouter;
